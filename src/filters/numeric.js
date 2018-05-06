a91559fa3883030144276dbd50741a5a89508779
Maksem Vovk <maksemvovk@gmail.com>
2018-05-06 19:12:07 +0300

    rm filter

--- src/filters/numeric.js
+++
@@ -1,3 +0,0 @@
-export default function (value) {
-  return /\D/g.test(value) ? '' : value
-}
--- src/index.js
+++ src/index.js
@@ -6,9 +6,6 @@ export default {
     const components = require.context('./components', true, /\.vue$/)
     components.keys().forEach(k => Vue.component(getModuleName(k), components(k).default))
 
-    const filters = require.context('./filters', true, /\.js$/)
-    filters.keys().forEach(k => Vue.filter(getModuleName(k), filters(k).default))
-
     Vue.directive('click-outside', ClickOutside)
   },
 }
